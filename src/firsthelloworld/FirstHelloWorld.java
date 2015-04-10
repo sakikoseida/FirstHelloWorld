2cf9376602233a342223f9c3280e391d72458a65
g13917kr <g13917kr@s30458.tsuda.ac.jp>
2015-04-10 10:51:37 +0900

    FirstHelloWorldです

---
+++ src/firsthelloworld/FirstHelloWorld.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package firsthelloworld;
+
+/**
+ *
+ * @author g13917kr
+ */
+public class FirstHelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+}
