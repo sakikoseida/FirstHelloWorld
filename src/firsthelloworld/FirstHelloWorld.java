2e611fa14906100d8d2160b8db63c6687066f846
g12948ss <g12948ss@s30454.tsuda.ac.jp>
2015-04-10 11:27:10 +0900

    コメントを追加しました。

--- src/firsthelloworld/FirstHelloWorld.java
+++ src/firsthelloworld/FirstHelloWorld.java
@@ -15,5 +15,6 @@ public class FirstHelloWorld {
      */
     public static void main(String[] args) {
         System.out.println("Hello, World");
+        System.out.println("Good Bye");
     }
 }
